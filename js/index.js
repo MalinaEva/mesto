05e4c8aed8f05e3c310affa3a398563a5578dae2
Dmitrieva Darya <teqi@yandex.ru>
2023-03-03 04:15:24 +0700

    Init

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const editBtn = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.popup__button-close');
+
+const profileTitle = document.querySelector('.profile__title');
+const profileText = document.querySelector('.profile__text');
+
+const popupName = document.querySelector('.popup__name');
+const popupDescription = document.querySelector('.popup__description');
+const popupForm = document.querySelector('.popup__form');
+
+const likeButtons = document.querySelectorAll('.element__button-like');
+
+editBtn.addEventListener('click', function () {
+    popupName.value = profileTitle.textContent;
+    popupDescription.value = profileText.textContent;
+    popup.classList.add('popup_opened');
+});
+
+closeBtn.addEventListener('click', function () {
+    popup.classList.remove('popup_opened');
+});
+
+popupForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    profileTitle.textContent = popupName.value;
+    profileText.textContent = popupDescription.value;
+    popup.classList.remove('popup_opened');
+});
+
+likeButtons.forEach(function (like) {
+    like.addEventListener('click', function () {
+        like.classList.toggle('element__button-like_active');
+    });
+})
